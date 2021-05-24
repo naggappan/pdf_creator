85af4623e81ffbd6ed76378bdd626d299dd43583
nishanthtofan <31266999+nishanthtofan@users.noreply.github.com>
2021-05-24 21:43:39 +0530

    added new document type (#1)
    added new document type

--- mysite/docwrite/views.py
+++ mysite/docwrite/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import SaleDeed
+from .models import Customer, SaleDeed
+from .models import SettlementDeed
 
 # Create your views here.
 br = "<br>"
@@ -18,5 +19,10 @@ def detail(request, customer_name):
     for i in AllSaleDeed:
         msg = msg + f"Later on {i.LaterDate}the said {i.SaidName} had sold the schedule of property/{i.Property} to one {i.To} for a valid sale consideration and possession was delivered as recited therein. The said document is duly stamped and registered as Doc.No.{i.DocNo} before {i.BeforeSRO} SRO. The said document is produced as document No.{i.DocNo} in the above list."
         msg = msg + f"{br}{br}"
-
+    AllSettlementDeed =SettlementDeed.objects.filter(Customer=customer_name)
+    msg: str = msg + f"Settlement Deed:{br}{br}"
+    for i in AllSettlementDeed:
+        msg = msg + f"Later on {i.LaterDate} the said {i.SaidName} executed settlement deed to his/her {i.ExecutedBy}(Relationship){i.Relation} out of love and affection in respect to the schedule of property/{i.Property} and possession was delivered as recited therein and the said deed was duly stamped and registered as Doc.No.{i.DocNo} before {i.BeforeSRO} SRO. The said Document is produced as document no.{i.DocNo}in the above list."
+        msg = msg + f"{br}{br}"
     return HttpResponse(msg)
+
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -73,11 +73,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'legal_doc',
+        'USER': 'admin',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
